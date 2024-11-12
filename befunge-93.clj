131db1eb4d60e04b4c3bd8d29d5243194a93dfb4
Martin <mguerrae@fi.uba.ar>
2024-11-12 00:24:33 -0300

    Función para hacer que la grilla se comporte como un toroide

--- befunge-93.clj
+++ befunge-93.clj
@@ -1,6 +1,13 @@
 (def width 80)
 (def height 25)
 
-;; Funcion para crear una grilla 80x25 con espacios
+;; funcion para crear una grilla 80x25 con espacios
 (defn crear-toroide []
   (vec (repeat height (vec (repeat width \space)))))
+
+(defn envolver-coord [x max]
+  (mod x max))
+
+;; funcion que envuelve las coordenadas si se pasan de los limites de la grilla
+(defn envolver-coords [x y]
+  [(wrap-coord x width) (wrap-coord y height)])
